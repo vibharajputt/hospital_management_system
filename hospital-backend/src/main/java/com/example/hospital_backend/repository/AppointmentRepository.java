8f3b8ab9f84c6fde61ecd1b13b743db8f5d57d83
Sudhanshu Kumar <160045794+sudhanshugks@users.noreply.github.com>
2026-04-07 21:23:14 +0530

    Update AppointmentRepository.java

---
+++ hospital-backend/src/main/java/com/example/hospital_backend/repository/AppointmentRepository.java
@@ -0,0 +1,11 @@
+package com.example.hospital_backend.repository;
+
+import com.example.hospital_backend.entity.Appointment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+    List<Appointment> findByPatientId(Long patientId);
+    List<Appointment> findByDoctorId(Long doctorId);
+}
